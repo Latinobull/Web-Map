cf159d5f509e3ba1edfedc5052c447f0fb4f8880
Donnahue George <Donnahuegjr@gmail.com>
2021-05-20 15:59:59 -0400

    adding a for loop for marker placement

--- map1.py
+++ map1.py
@@ -2,17 +2,22 @@ import folium
 
 map = folium.Map(
     location=[40.639050998016664, -73.92169274478333],
-    zoom_start=6,
+    zoom_start=10,
     tiles="OpenStreetMap",
 )
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(
-    folium.Marker(
-        location=[40.64748489005272, -73.88336934270934],
-        popup="Hi I am a Marker",
-        icon=folium.Icon(color="red"),
+for coords in [
+    [40.64748489005272, -73.88336934270934],
+    [40.60204651406353, -73.96563805750966],
+]:
+
+    fg.add_child(
+        folium.Marker(
+            location=coords,
+            popup="Hi I am a Marker",
+            icon=folium.Icon(color="red"),
+        )
     )
-)
 
 map.add_child(fg)
 map.save("Map1.html")
