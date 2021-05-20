ccb0be0a2e22fe4521804fd47e6e0921cbdb51cc
Donnahue George <Donnahuegjr@gmail.com>
2021-05-20 16:20:02 -0400

    adding names of each volcano

--- map1.py
+++ map1.py
@@ -4,19 +4,20 @@ import pandas as pd
 data = pd.read_csv("Volcanoes.txt")
 lon = list(data["LON"])
 lat = list(data["LAT"])
+name = list(data["NAME"])
 
 map = folium.Map(
-    location=[40.639050998016664, -73.92169274478333],
-    zoom_start=10,
+    location=[37.97257545638386, -98.89051431454878],
+    zoom_start=5,
     tiles="OpenStreetMap",
 )
 fg = folium.FeatureGroup(name="My Map")
-for lt, ln in zip(lat, lon):
+for lt, ln, nm in zip(lat, lon, name):
 
     fg.add_child(
         folium.Marker(
             location=[lt, ln],
-            popup="Hi I am a Marker",
+            popup="Volcano {}".format(nm),
             icon=folium.Icon(color="red"),
         )
     )
