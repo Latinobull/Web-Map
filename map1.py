060597d6f315c7c8efc38909e0746e37cc120301
Donnahue George <Donnahuegjr@gmail.com>
2021-05-20 15:48:19 -0400

    changed the map

--- map1.py
+++ map1.py
@@ -1,4 +1,8 @@
 import folium
 
-map = folium.Map(location=[40.639050998016664, -73.92169274478333], zoom_start=6)
+map = folium.Map(
+    location=[40.639050998016664, -73.92169274478333],
+    zoom_start=6,
+    tiles="OpenStreetMap",
+)
 map.save("Map1.html")
